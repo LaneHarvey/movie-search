a1ff49e379b2e9124fc67ee84a70e2cd65e47b72
Lane Harvey <mrlaneharvey@gmail.com>
2020-05-27 14:51:31 -0600

    fixed apikey

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -10,7 +10,7 @@ export default function SearchMovies(){
     const searchMovies = async (e) => {
         e.preventDefault();
 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=677838a8e83f24abcb6daa92dc92707e0&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=677838a8e83f24abcb6daa92dc92707e&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
